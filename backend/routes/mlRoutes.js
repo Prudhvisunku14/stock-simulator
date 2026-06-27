08f91cc6fce7e6eda0594894160ebbc321e7edf6
Prudhvi-IITM <prudhvisunku@gmail.com>
2026-06-28 01:16:58 +0530

    Update mlRoutes.js

--- backend/routes/mlRoutes.js
+++ backend/routes/mlRoutes.js
@@ -1,10 +1,8 @@
-// routes/mlRoutes.js
 const express = require('express');
 const router = express.Router();
 const { predict, getPatterns } = require('../controllers/mlController');
-const authMiddleware = require('../middleware/auth');
 
-router.post('/predict', authMiddleware, predict);
-router.get('/patterns/:stock_symbol', authMiddleware, getPatterns);
+router.post('/predict', predict);
+router.get('/patterns/:stock_symbol', getPatterns);
 
 module.exports = router;
